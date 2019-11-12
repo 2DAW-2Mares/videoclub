1962d702542a512238f845aef36b9fc8e8afa44f
Sergio Sánchez <sergioavileses@gmail.com>
2019-11-12 09:00:55 +0100

    Solución ejercicio2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,40 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/login', function () {
+    $conectado="";
+    $conectado = "conectado";
+    return $conectado;
+});
+Route::get('/logout', function () {
+    $conectado="";
+    $conectado = "desconectado";
+    return $conectado;
+});
+
+Route::get('/catalog', function () {
+    $conectado="";
+    $conectado = "Listado peliculas";
+    return $conectado;
+});
+Route::get('/catalog', function () {
+    $conectado="";
+    $conectado = "Listado peliculas";
+    return $conectado;
+});
+Route::get('/catalog/show/{id}', function ($id) {
+    $conectado="";
+    $conectado = "Listado peliculas".$id;
+    return $conectado;
+});
+Route::get('/create', function () {
+    $conectado="";
+    $conectado = "Añadir pelicula";
+    return $conectado;
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    $conectado="";
+    $conectado = "Modificar pelicula".$id;
+    return $conectado;
+});
