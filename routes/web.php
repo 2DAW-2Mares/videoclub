47b1ae0d692dfd05bed9989f5e8c1d425db403fe
unknown <7476903@alu.murciaeduca.es>
2019-11-12 08:44:30 +0100

    Solucion ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+Route::get('/', function () {
+    return "Pantalla principal";
+});
+Route::get('/login', function () {
+    return "Login usuario";
+});
+Route::get('/logout', function () {
+    return "Logout usuario";
+});
+Route::get('/catalog', function () {
+    return "Listado películas";
+});
+Route::get('/catalog/show/{id}', function ($id) {
+    return "Vista detalle película $id";
+});
+Route::get('/catalog/create', function () {
+    return "Añadir película";
+});
+Route::get('/catalog/edit/{id}', function ($id) {
+    return "Vista detalle película $id";
 });
