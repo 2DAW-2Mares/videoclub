080386a8f61aca39a4350a5c5f90d3991e8a3e97
Mario <mariovillaescusa@hotmail.com>
2019-11-12 08:39:09 +0100

    Solucion a los ejercicios I

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,29 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return "Login usuario.";
+});
+
+Route::get('/logout', function () {
+    return "Logout usuario.";
+});
+
+Route::get('/catalog', function () {
+    return "Listado de películas.";
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return "Vista detalle película " . $id;
+})
+->where('numero', '[0-9]+');
+
+Route::get('/catalog/create', function () {
+    return "Añadir película";
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return "Modificar película " . $id;
+})
+->where('numero', '[0-9]+');
