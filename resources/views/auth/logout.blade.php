a645c1a070c9f4a8311eaa1c204c6bddc6193dc5
victor <victor.vax.58@gmail.com>
2019-11-12 09:22:29 +0100

    Solucion ej 4

---
+++ resources/views/auth/logout.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+
+    Logout usuario
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -12,29 +12,29 @@
 */
 
 Route::get('/', function () {
-    return "Pagina principal";
+    return view('home');
 });
 
 Route::get('/login', function () {
-    return "Login usuario";
+    return view('auth.login');
 });
 
 Route::get('/logout', function () {
-    return "Logout usuario";
+    return view('auth.logout');
 });
 
 Route::get('/catalog', function () {
-    return "Listado peliculas";
+    return view('catalog.index');
 });
 
 Route::get('/catalog/show/{id}', function ($id) {
-    return "Vista detalle pelicula $id";
+    return view('catalog.show', array('id' => $id));
 });
 
 Route::get('/catalog/create', function () {
-    return "Añadir pelicula";
+    return view('catalog.create');
 });
 
 Route::get('/catalog/edit/{id}', function ($id) {
-    return "Modificar pelicula $id";
+    return view('catalog.edit', array('id' => $id));
 });
