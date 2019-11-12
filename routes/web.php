5554bdf3021f9f61698695ec776e2ec4445d66a8
Marcfernic <1642969@alu.murciaeduca.es>
2019-11-12 08:45:42 +0100

    Solución a los ejercicios Definicion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,29 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Pantalla Principal";
+});
+
+Route::get('login', function () {
+    return "Login usuario";
+});
+
+Route::get('logout', function () {
+    return "Logout usuario";
+});
+
+Route::get('catalog', function () {
+    return "Listado películas";
+});
+
+Route::get('catalog/show/{id}', function ($id) {
+    return "Vista detalle película ".$id;
+});
+
+Route::get('catalog/create', function () {
+    return "Añadir película";
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    return "Modificar película ".$id;
 });
