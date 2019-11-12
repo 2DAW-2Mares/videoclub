a4a2dd7ca91a7e9984051c75675dab9a292ebd82
unknown <ghonti67@gmail.com>
2019-11-12 12:46:49 +0100

    Solución al ejercicio 2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return ('hola');
+});
+
+Route::get('/logout', function () {
+    return ('adios');
+});
+
+Route::get('/catalog', function () {
+    return ('Catalogo de peliculas');
+});
+
+Route::get('/catalog/show/{id}', function ($id) {
+    return ('Pelicula de id ' . $id);
+})
+->where('id', '[0-9]+');
+
+Route::get('/catalog', function () {
+    return ('Crear catálogo');
+});
+
+Route::get('/catalog/edit/{id}', function ($id) {
+    return ('Vas a editar la peli del id ' . $id);
+})
+->where('id', '[0-9]+');
+
