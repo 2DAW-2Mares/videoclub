46cb32cbb41a99306b1a6be6045d4bda1b93709f
Alberto Sierra <alberto.sierra@murciaeduca.es>
2019-11-13 21:40:18 +0100

    Solución a ejerciciosII

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -2,6 +2,21 @@
 
 @section('content')
 
-    Listado películas
+    <div class="row">
+
+        @foreach( $arrayPeliculas as $key => $pelicula )
+            <div class="col-xs-6 col-sm-4 col-md-3 text-center">
+
+                <a href="{{ url('/catalog/show/' . $key ) }}">
+                    <img src="{{$pelicula['poster']}}" style="height:200px"/>
+                    <h4 style="min-height:45px;margin:5px 0 10px 0">
+                        {{$pelicula['title']}}
+                    </h4>
+                </a>
+
+            </div>
+        @endforeach
+
+    </div>
 
 @stop
