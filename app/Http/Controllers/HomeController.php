f66dacac76a551ad552f7649528149e1ffb6481a
Unimusha <Alextopota528@gmail.com>
2019-11-19 09:08:27 +0100

    Ejercicio1

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller{
+    public function getHome(){
+    return view('home');
+}
+}
--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::get('/', 'HomeController@getHome');
+
 Route::get('login', function () {
     return view('auth.login');
 });
 Route::get('logout', function () {
     return view('auth.logout');
 });
+/*
 Route::get('catalog', function () {
     return view('catalog.index');
-});
+});*/
+Route::get('/catalog', 'CatalogController@getIndex');
+
+/*
 Route::get('catalog/show/{id}', function ($id) {
     return view('catalog.show', array('id'=>$id));
-})->where('id', '[0-9]+');
+})->where('id', '[0-9]+');*/
+Route::get('/catalog/show/{id}', 'CatalogController@getshow');
+/*
 Route::get('catalog/create', function () {
     return view('catalog.create');
-});
+});*/
+Route::get('/catalog/create', 'CatalogController@getCreate');
+
+/*
 Route::get('catalog/edit/{id}', function ($id) {
     return view('catalog.edit', array('id'=>$id));
-})->where('id', '[0-9]+');
+});*/
+Route::get('/catalog/edit/{id}', 'CatalogController@getEdit')
+->where('id', '[0-9]+');
