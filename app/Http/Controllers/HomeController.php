8aa3467d5a363b5293804f66b9942bd771e8cabd
Mario <mariovillaescusa@hotmail.com>
2019-11-19 10:06:32 +0100

    Ejercicio 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function getHome(){
-        return view('home');
+        return redirect()->action('CatalogController@getIndex');
     }
 }
--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,6 +2,36 @@
 
 @section('content')
 
-    Vista detalle película {{ $id }}
+<div class="row">
+
+<div class="col-sm-4">
+
+<img src="{{$pelicula['poster']}}" style="width:350px; height:auto"></img>
+
+</div>
+<div class="col-sm-8">
+
+<h2>{{ $pelicula['title'] }}</h2>
+<h5>Año: {{ $pelicula['year'] }}</h5>
+<h5>Director: {{ $pelicula['director'] }}</h5>
+<p>Resumen: {{ $pelicula['synopsis'] }}</p>
+@if ( $pelicula['rented'] )
+    <p>Estado: Película actualmente alquilada</p>
+    <button type="button" class="btn btn-danger">Devolver película</button>
+@else
+    <p>Estado: Película disponible</p>
+    <button type="button" class="btn btn-primary">Alquilar película</button>
+@endif
+<button type="button" class="btn btn-warning">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Editar película
+</button>
+<button type="button" class="btn btn-default">
+<span class="glyphicon glyphicon-star" aria-hidden="true"></span> Volver al listado
+</button>
+
+
+</div>
+</div>
+
 
 @stop
