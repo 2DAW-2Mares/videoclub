7e327b053ebda580e9d5ec3ee030f2e8e240fc0a
unknown <ghonti67@gmail.com>
2019-11-19 12:37:40 +0100

    EjercicioI hecho

---
+++ app/Http/Controllers/CatalogController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    public function getIndex()
+    {
+        return view('catalog.index');
+    }
+    public function getCreate()
+    {
+        return view('catalog.create');
+    }
+    public function getShow($id)
+    {
+        return view('catalog.show', array('id'=>$id));
+    }
+    public function getEdit($id)
+{
+    return view('catalog.edit', array('id'=>$id));
+}
+}
