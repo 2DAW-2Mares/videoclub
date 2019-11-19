019934b55da5aa8baaa77c24d709cbf5996e8bda
HectorDawDosMares <7946424@alu.murciaeduca.es>
2019-11-19 10:21:16 +0100

    botones editar y volver

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -12,7 +12,7 @@
         </div>
         <div class="col-sm-8">
 
-            {{-- TODO: Datos de la película --}}
+            {{-- READY: Datos de la película --}}
             <h1>{{ $id['title'] }}</h1>
             <h4>Año: {{ $id['year'] }}</h4>
             <h4>Director: {{ $id['director'] }}</h4>
@@ -29,9 +29,10 @@
                 <p><strong>Estado:</strong> Película disponible.
                 </p>
                 <button type="button" class="btn btn-primary">Alquilar Película</button>
-
             @endif
-
+            {{-- TODO: Iconos botones --}}
+            <button type="button" class="btn btn-warning">Editar Película</button>
+            <button type="button" class="btn btn-default" onclick="window.location.href='http://videoclub.test/'">Volver al listado</button>
         </div>
     </div>
 
