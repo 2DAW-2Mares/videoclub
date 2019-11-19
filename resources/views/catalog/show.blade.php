84ff3452a17c89e0470eb29de72185900874c378
AlejandroSanchez <7963289@alu.murciaeduca.es>
2019-11-19 18:20:46 +0100

    Solucion a los ejerciciosII

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,6 +2,42 @@
 
 @section('content')
 
-    Vista detalle película {{ $id }}
+<div class="row">
+
+    <div class="col-sm-4">
+        
+    <img src={{$arrayPeliculas['poster']}} alt="sd">
+
+    </div>
+    <div class="col-sm-8">
+
+    <h1>{{$arrayPeliculas['title']}}</h1>
+    <h3>Año :  {{$arrayPeliculas['year']}}</h2>
+    <h3>Director : {{$arrayPeliculas['director']}}</h3>
+
+    <br>
+
+    <p><b>Resumen :</b> {{$arrayPeliculas['synopsis']}} </p>
+    <br>
+        
+    @if($arrayPeliculas['rented'] == true)
+    <p><b>Estado:</b>Pelicula actualmente alquilada</p>
+        <button class="btn btn-danger">Devoler Pelicula</button>
+        @endif
+
+        @if($arrayPeliculas['rented'] == false)
+        <p><b>Estado:</b>Pelicula disponible</p>
+        <button class="btn btn-primary">Alquilar Pelicula</button>
+        @endif
+        <a href="../../catalog/create/{{$id}}" class="btn btn-warning">Editar Pelicula</a>
+        <a href="../../catalog/" class="btn btn-outline-dark">Volver al listado</a>
+        
+
+
+
+    </div>
+</div>
+
+
 
 @stop
--- routes/web.php
+++ routes/web.php
@@ -11,24 +11,30 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+/* 
+
+Route::get('tabla','NumerosController@showFormTabla');
+
+*/
+
+Route::get('/', 'HomeController@getHome');
+
+//----------------------------
 Route::get('login', function () {
     return view('auth.login');
 });
 Route::get('logout', function () {
     return view('auth.logout');
 });
-Route::get('catalog', function () {
-    return view('catalog.index');
-});
-Route::get('catalog/show/{id}', function ($id) {
-    return view('catalog.show', array('id'=>$id));
-})->where('id', '[0-9]+');
-Route::get('catalog/create', function () {
-    return view('catalog.create');
-});
-Route::get('catalog/edit/{id}', function ($id) {
-    return view('catalog.edit', array('id'=>$id));
-})->where('id', '[0-9]+');
+
+//-----------------------------
+
+Route::get('catalog', 'CatalogController@getIndex');
+
+//Route::get('tabla/{numero}','NumerosController@showTablaMultiplicar')
+
+Route::get('catalog/show/{id}', 'CatalogController@getShow');
+
+Route::get('catalog/create', 'CatalogController@getCreate');
+
+Route::get('catalog/create/{id}', 'CatalogController@getEdit');
