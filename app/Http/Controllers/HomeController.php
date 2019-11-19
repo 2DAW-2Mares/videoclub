9116b635bd6f3cda1884c110b3d2a3eee6e32c37
AntonioLV1999 <1832577@alu.murciaeduca.es>
2019-11-19 09:47:02 +0100

    Ejercicio 2 Incompleto

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function getHome(){
-        return view('home');
+        //return view('home');
+        return redirect()->action('CatalogController@getIndex');
     }
 }
