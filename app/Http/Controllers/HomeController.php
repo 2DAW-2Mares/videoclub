b479246c234e6246400687ed9e7609941f459b76
HectorDawDosMares <7946424@alu.murciaeduca.es>
2019-11-19 09:43:18 +0100

    Controladores - Ej02

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends Controller
     }
 
     public function getHome() {
-        return view('home');
+        return redirect()->action('CatalogController@getIndex');
     }
 }
--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,6 +2,37 @@
 
 @section('content')
 
-    Vista detalle película {{ $id }}
+    <div class="row">
+
+        <div class="col-sm-4">
+
+            {{-- <img src="{{ $id['poster'] }}"> --}}
+            <img src="https://m.media-amazon.com/images/M/MV5BMTM5MzExYjAtMjY3MS00MWYyLTlmZDItMWQzNjU5NDMxNGFkXkEyXkFqcGdeQXVyNzYzMjEyMjc@._V1_UX182_CR0,0,182,268_AL_.jpg">
+
+        </div>
+        <div class="col-sm-8">
+
+            {{-- TODO: Datos de la película --}}
+            <h1>{{ $id['title'] }}</h1>
+            <h4>Año: {{ $id['year'] }}</h4>
+            <h4>Director: {{ $id['director'] }}</h4>
+            <p>
+                <strong>Resumen:</strong>
+                {{ $id['synopsis'] }}
+            </p>
+
+            @if($id['rented'])
+                <p><strong>Estado:</strong> Película actualmente alquilada.
+                </p>
+            <button type="button" class="btn btn-danger">Devolver película</button>
+            @else
+                <p><strong>Estado:</strong> Película disponible.
+                </p>
+                <button type="button" class="btn btn-primary">Alquilar Película</button>
+
+            @endif
+
+        </div>
+    </div>
 
 @stop
