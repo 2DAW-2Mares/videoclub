8bdad5107c43328fe6e5ad0533604f8fe33f6118
unknown <7476903@alu.murciaeduca.es>
2019-11-20 13:06:00 +0100

    fin ejercicio2

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,6 +2,29 @@
 
 @section('content')
 
-    Vista detalle película {{ $id }}
+    {{-- Vista detalle película {{ $id }} --}}
+    <div class="row">
 
+        <div class="col-sm-4">
+        <img src="{{$arrayPeliculas['poster']}}" style="height:200px">
+
+        </div>
+        <div class="col-sm-8">
+        <h1>{{$arrayPeliculas['title']}}</h1>
+        <p>Año: {{$arrayPeliculas['year']}}</p>
+        <p>Director: {{$arrayPeliculas['director']}}</p>
+        <p>Resumen: {{$arrayPeliculas['synopsis']}}</p>
+
+        @if ($arrayPeliculas['rented'] == true)
+
+        <p> Estado : Pelicula disponible</p>
+        <button class="btn btn-primary">Alquilar pelicula</button>
+        @elseif ($arrayPeliculas['rented'] == false)
+        <p> Estado : Película actualmente alquilada</p>
+        <button class="btn btn-danger">Devolver pelicula</button>
+        @endif
+        <button class="btn btn-warning">Editar pelicula</button>
+        <button class="btn btn-default">Volver al listado</button>
+        </div>
+    </div>
 @stop
