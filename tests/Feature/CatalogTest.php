efe82e39251e62b8aaf36d4ffba1a2c8efecec58
Alberto Sierra <alberto.sierra@murciaeduca.es>
2019-11-20 07:28:01 +0000

    Test para la segunda tanda de ejercicios

--- tests/Feature/CatalogTest.php
+++ tests/Feature/CatalogTest.php
@@ -5,48 +5,85 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Http\Controllers\CatalogController;
 
 class CatalogTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    const CATALOG_PATH = 'catalog';
+
+    public function testHome()
     {
-        $basePath = 'catalog';
-        $idRand = rand(1,10);
-        $requests = array(
+        $response = $this->get('/');
+        $response->assertRedirect(action('CatalogController@getIndex'));
+    }
+
+    public function testCatalogShow()
+    {
+        $catalogController = new CatalogController();
+        $id = 0;
+        foreach ($catalogController->arrayPeliculas as $pelicula) {
+            $response = $this->get(self::CATALOG_PATH . '/show/' . $id);
+            $response->assertViewIs('catalog.show');
+            $response->assertStatus(200);
+            $texto1 = $pelicula['rented'] ? 'alquilada' : 'disponible';
+            $texto2 = $pelicula['rented'] ? 'Devolver' : 'Alquilar';
+            $response->assertSeeText($texto1);
+            $response->assertSeeText($texto2);
+            $id++;
+        }
+    }
+
+    public function testCatalogViews()
+    {
+        $catalogController = new CatalogController();
+        $idRand = rand(1,count($catalogController->arrayPeliculas) - 1);
+        $textosPeliculas = array();
+
+        foreach ($catalogController->arrayPeliculas as $pelicula) {
+            array_push($textosPeliculas, $pelicula['title']);
+        }
+
+        $requestsCatalog = array(
             array(
                 'path' => '/',
-                'text' => 'Listado películas'
-            ),
-            array(
-                'path' => '/show/' . $idRand,
-                'text' => 'Vista detalle película ' . $idRand
+                'view' => 'catalog.index',
+                'texts' => $textosPeliculas
             ),
             array(
                 'path' => '/create',
-                'text' => 'Añadir película'
+                'view' => 'catalog.create',
+                'texts' => array(
+                    'Añadir película',
+                    'Resumen'
+                )
             ),
             array(
                 'path' => '/edit/' . $idRand,
-                'text' => 'Modificar película ' . $idRand
+                'view' => 'catalog.edit',
+                'texts' => array(
+                    'Modificar película',
+                    'Título',
+                    'Resumen'
+                )
             ),
         );
 
-        foreach ($requests as $request) {
-            $response = $this->get($basePath . $request['path']);
-
-            $response->assertStatus(200);
-            $response->assertSeeText($request['text']);
+        foreach ($requestsCatalog as $request) {
+            $response = $this->get(self::CATALOG_PATH . $request['path']);
+            $response->assertSuccessful();
+            $response->assertViewIs($request['view']);
+            foreach ($request['texts'] as $text) {
+                $response->assertSeeText($text);
+            }
         }
 
-        // Controlar que el id sea numérico
-        $response = $this->get($basePath . '/edit/e');
-
-        $response->assertStatus(404);
-
     }
+
+    public function testEdit404()
+    {
+        // Controlar que el id sea numérico
+        $response = $this->get(self::CATALOG_PATH . '/edit/e');
+        $response->assertStatus(404);
+    }
+
 }
