49217eaa34db47beefd970efe428f9e53d66e379
Alberto Sierra <alberto.sierra@murciaeduca.es>
2019-11-25 19:11:11 +0100

    Solución a la tercera tanda de ejercicios

---
+++ database/migrations/2019_11_25_173229_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->year('year', 8);
+            $table->string('director', 64);
+            $table->string('poster');
+            $table->boolean('rented')->default(false);
+            $table->text('synopsis');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
