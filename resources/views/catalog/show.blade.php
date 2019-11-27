b816101790c3ba64fa157895f8cb0422291ece7d
unknown <7476903@alu.murciaeduca.es>
2019-11-27 12:43:18 +0100

    Ejercicio3 terminado

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -7,25 +7,25 @@
         <div class="col-sm-4">
 
             <a href="{{ url('/catalog/show/' . $id ) }}">
-                <img src="{{$pelicula['poster']}}" style="height:200px"/>
+                <img src="{{$pelicula->poster}}" style="height:200px"/>
             </a>
 
         </div>
         <div class="col-sm-8">
 
-            <h4>{{$pelicula['title']}}</h4>
-            <h6>A&ntilde;o: {{$pelicula['year']}}</h6>
-            <h6>Director: {{$pelicula['director']}}</h6>
-            <p><strong>Resumen:</strong> {{$pelicula['synopsis']}}</p>
+            <h4>{{$pelicula->title}}</h4>
+            <h6>A&ntilde;o: {{$pelicula->year}}</h6>
+            <h6>Director: {{$pelicula->director}}</h6>
+            <p><strong>Resumen:</strong> {{$pelicula->synopsis}}</p>
             <p><strong>Estado: </strong>
-                @if($pelicula['rented'])
+                @if($pelicula->rented)
                     Pel&iacute;cula actualmente alquilada.
                 @else
                     Pel&iacute;cula disponible.
                 @endif
             </p>
 
-            @if($pelicula['rented'])
+            @if($pelicula->rented)
                 <a class="btn btn-danger" href="#">Devolver pel&iacute;cula</a>
             @else
                 <a class="btn btn-primary" href="#">Alquilar pel&iacute;cula</a>
