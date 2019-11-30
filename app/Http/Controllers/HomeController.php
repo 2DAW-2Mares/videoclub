7694fd32710b4e94385ac015b0221a843ad9dbb3
HectorDawDosMares <7946424@alu.murciaeduca.es>
2019-11-30 19:33:54 +0100

    sistema de autenticacion y adaptar el layout

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,9 +6,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function getHome()
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
     {
-        return redirect()->action('CatalogController@getIndex');
+        $this->middleware('auth');
     }
 
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
 }
