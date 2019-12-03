fb1462f6e944597d41dcb424ef0eba8cd98f5bbf
jasbi06 <1832517@alu.murciaeduca.es>
2019-12-03 08:38:43 +0100

    Ejercicio IV Create y Edit

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -29,11 +29,36 @@ class CatalogController extends Controller
         return view('catalog.create');
     }
 
+    public function postCreate(Request $request)
+    {
+        $pelicula = new Movie();
+        $pelicula ->title = $request->title;
+        $pelicula ->year = $request->year;
+        $pelicula ->director = $request->director;
+        $pelicula ->poster = $request->poster;
+        $pelicula ->synopsis = $request->synopsis;
+        $pelicula->save();
+        return redirect('/catalog');
+    }
+
     public function getEdit($id)
     {
         $pelicula = Movie::findOrFail($id);
         return view('catalog.edit', array(
             'pelicula' => $pelicula
         ));
+    }
+
+    public function putEdit(Request $request,$id)
+    {
+        $pelicula = Movie::findOrFail($id);
+        $pelicula ->title = $request->title;
+        $pelicula ->year = $request->year;
+        $pelicula ->director = $request->director;
+        $pelicula ->poster = $request->poster;
+        $pelicula ->synopsis = $request->synopsis;
+        $pelicula->save();
+        return redirect('/catalog.peliculaEditada');
+        //return view('sumaResultado', array('sumando1' => $request->input('sumando1'),'sumando2' => $request->input('sumando2')));
     }
 }
