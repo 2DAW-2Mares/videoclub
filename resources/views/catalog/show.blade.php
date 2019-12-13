b023b0f45f909fae936397f380243773c4fa9526
Unimusha <Alextopota528@gmail.com>
2019-12-13 11:42:36 +0100

    Resource Controller

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -2,46 +2,54 @@
 
 @section('content')
 
-    <div class="row">
+<div class="row">
 
-        <div class="col-sm-4">
+    <div class="col-sm-4">
 
-            <a href="{{ url('/catalog/show/' . $pelicula->id ) }}">
-                <img src="{{$pelicula->poster}}" style="height:200px"/>
-            </a>
+        <a href="{{ url('/movie/' . $pelicula->id ) }}">
+            <img src="{{$pelicula->poster}}" style="height:200px" />
+        </a>
 
-        </div>
-        <div class="col-sm-8">
-
-            <h4>{{$pelicula->title}}</h4>
-            <h6>A&ntilde;o: {{$pelicula->year}}</h6>
-            <h6>Director: {{$pelicula->director}}</h6>
-            <p><strong>Resumen:</strong> {{$pelicula->synopsis}}</p>
-            <p><strong>Estado: </strong>
-                @if($pelicula->rented)
-                    Pel&iacute;cula actualmente alquilada.
-                @else
-                    Pel&iacute;cula disponible.
-                @endif
-            </p>
-
-            <form action="{{ action('CatalogController@changeRented') }}" method="POST">
-                {{method_field('PUT')}}
-                @csrf
-                <input type="hidden" name="id" value="{{ $pelicula->id }}">
-
-                @if($pelicula->rented)
-                <button type="submit" class="btn btn-danger">Devolver pel&iacute;cula</button>
-                @else
-                <button type="submit" class="btn btn-primary">Alquilar pel&iacute;cula</button>
-                @endif
-
-                <a class="btn btn-warning" href="{{ url('/catalog/edit/' . $pelicula->id ) }}">
-                    <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
-                    Editar pel&iacute;cula</a>
-                <a class="btn btn-outline-info" href="{{ action('CatalogController@getIndex') }}">Volver al listado</a>
-            </form>
-        </div>
     </div>
+    <div class="col-sm-8">
+
+        <h4>{{$pelicula->title}}</h4>
+        <h6>A&ntilde;o: {{$pelicula->year}}</h6>
+        <h6>Director: {{$pelicula->director}}</h6>
+        <p><strong>Resumen:</strong> {{$pelicula->synopsis}}</p>
+        <p><strong>Estado: </strong>
+            @if($pelicula->rented)
+            Pel&iacute;cula actualmente alquilada.
+            @else
+            Pel&iacute;cula disponible.
+            @endif
+        </p>
+
+        <form action="{{ action('MovieController@changeRented') }}" method="POST">
+            {{method_field('PUT')}}
+            @csrf
+            <input type="hidden" name="id" value="{{ $pelicula->id }}">
+
+            @if($pelicula->rented)
+            <button type="submit" class="btn btn-danger">Devolver pel&iacute;cula</button>
+            @else
+            <button type="submit" class="btn btn-primary">Alquilar pel&iacute;cula</button>
+            @endif
+    </form>
+            <a class="btn btn-success" href=" {{ action('MovieController@edit', array("movie" =>$pelicula)) }} ">
+                <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
+                Editar pel&iacute;cula</a>
+            <form action="{{ action('MovieController@destroy', array("movie" =>$pelicula)) }}" method="POST">
+                {{method_field('DELETE')}}
+                 @csrf
+                <button onclick="return confirm('Esta seguro de borrar')" type="submit" class="btn btn-warning">Borrar</button>
+            </form>
+            
+            <a class="btn btn-outline-info" href="{{ action('MovieController@index') }}">Volver al listado</a>
+
+
+    
+    </div>
+</div>
 
 @stop
--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,15 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'HomeController@getHome');
-
-Route::group(['prefix' => 'catalog', 'middleware' => 'auth'], function() {
-    Route::get('/', 'CatalogController@getIndex');
-
-    Route::get('show/{id}', 'CatalogController@getShow')->where('id', '[0-9]+');
-
-    Route::get('create', 'CatalogController@getCreate');
-    Route::post('create', 'CatalogController@postCreate');
-
-    Route::get('edit/{id}', 'CatalogController@getEdit')->where('id', '[0-9]+');
-    Route::put('edit', 'CatalogController@putEdit');
-
-    Route::put('/changeRented', 'CatalogController@changeRented');
-});
+Route::get('/home', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('movie', 'MovieController');
+    Route::put('/changeRented', 'MovieController@changeRented');
+});
+
