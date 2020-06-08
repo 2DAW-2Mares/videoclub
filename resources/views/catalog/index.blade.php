7ddb35a9e65b00bb950b11942bab32e13c06978a
Alberto Sierra <alberto.sierra@murciaeduca.es>
2020-06-08 22:41:23 +0200

    Formulario de búsqueda

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="row" style="margin-bottom:20px">
+        <div class="offset-md-3 col-md-6">
+            <div class="card">
+                <div class="card-header text-center">
+                    Buscar película
+                </div>
+                <div class="card-body" style="padding:30px">
+
+                    <form action="{{ action('CatalogController@getIndex') }}" method="POST">
+
+                        @csrf
+
+                        <div class="form-group">
+                            <input type="text" name="busqueda" id="busqueda" class="form-control" value="" placeholder="Escriba el texto a buscar en el título">
+                        </div>
+
+                        <div class="form-group text-center">
+                            <button type="submit" class="btn btn-primary" style="padding:8px 100px;margin-top:25px;">
+                                Buscar
+                            </button>
+                        </div>
+
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="row">
 
